6a4e2000d7d4b3f95dd40ca90d689821569f819e
Rodney Barnes <rdnybrns@gmail.com>
2019-08-05 15:19:46 -0400

    Installed the MaterialUI Icons pack. Started the Layout component.

--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -7,41 +7,158 @@
 
 import React from "react"
 import PropTypes from "prop-types"
+import {
+  AppBar,
+  Divider,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@material-ui/core"
+import MenuIcon from "@material-ui/icons/Menu"
+import { makeStyles, useTheme } from "@material-ui/core/styles"
 import { useStaticQuery, graphql } from "gatsby"
 
-import Header from "./header"
 import "./layout.css"
+import { inherits } from "util"
+
+const drawerWidth = 240
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    backgroundColor: "#0c0a05",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}))
+
+/**
+ * This is the Layout component, which will be called in all child components to provide
+ * a unified style to the website.
+ *
+ * Much of this component has been drawn from the Material UI documentation for a responsive drawer,
+ * which has a permanent drawer for tablet- and desktop-sized pages, and then a temporary drawer
+ * for mobile pages.
+ *
+ * Reference: https://material-ui.com/components/drawers/#responsive-drawer
+ */
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  const classes = useStyles()
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Typography variant="h6">THE NEW DEVELOPERS</Typography>
+      <Divider />
+      <List>
+        {/* 
+          Eventually the below logic should be replaced by a GraphQL query that pulls information
+          for all pages and then programmatically populates them as list items with links.
+         */}
+        {["Join our mailing list", "Events", "Mentorship", "Opportunities"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
+    </div>
+  )
 
   return (
-    <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
-        <main>{children}</main>
+    <div className={classes.root}>
+      <Hidden smUp implementation="css">
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              THE NEW DEVELOPERS
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Hidden>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor="left"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+      <div>
+        <main className={classes.content}>{children}</main>
         <footer>
           © {new Date().getFullYear()}, Built with
           {` `}
           <a href="https://www.gatsbyjs.org">Gatsby</a>
         </footer>
       </div>
-    </>
+    </div>
   )
 }
 

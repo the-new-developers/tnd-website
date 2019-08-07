150bc9fc5b8135652a04e04587f0f790208d70f2
Rodney Barnes <rdnybrns@gmail.com>
2019-08-06 21:07:26 -0400

    Moved drawer into separate component. Adjusted global styles.

--- website/src/components/header.js
+++ website/src/components/header.js
@@ -1,42 +1,147 @@
 import { Link } from "gatsby"
-import PropTypes from "prop-types"
 import React from "react"
+import {
+  AppBar,
+  Divider,
+  Drawer,
+  Hidden,
+  IconButton,
+  List,
+  ListItem,
+  ListItemText,
+  Toolbar,
+  Typography,
+} from "@material-ui/core"
+import { Menu } from "@material-ui/icons"
+import { makeStyles } from "@material-ui/core/styles"
 
-const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
-      </h1>
+const drawerWidth = 240
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    display: "flex",
+    backgroundColor: "#0c0a05",
+  },
+  paper: {
+    background: "#0c0a05",
+  },
+  drawer: {
+    [theme.breakpoints.up("sm")]: {
+      width: drawerWidth,
+      flexShrink: 0,
+    },
+  },
+  appBar: {
+    marginLeft: drawerWidth,
+    [theme.breakpoints.up("sm")]: {
+      width: `calc(100% - ${drawerWidth}px)`,
+    },
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+    [theme.breakpoints.up("sm")]: {
+      display: "none",
+    },
+  },
+  toolbar: theme.mixins.toolbar,
+  drawerPaper: {
+    width: drawerWidth,
+  },
+  content: {
+    flexGrow: 1,
+    padding: theme.spacing(3),
+  },
+}))
+
+/**
+ * This component contains the logic for the top app bar and the navigation drawer.
+ *
+ * Much of this component has been drawn from the Material UI documentation for a responsive drawer,
+ * which has a permanent drawer for tablet- and desktop-sized pages, and then a temporary drawer
+ * for mobile pages.
+ *
+ * Reference: https://material-ui.com/components/drawers/#responsive-drawer
+ */
+const Header = () => {
+  const classes = useStyles()
+  const [mobileOpen, setMobileOpen] = React.useState(false)
+
+  function handleDrawerToggle() {
+    setMobileOpen(!mobileOpen)
+  }
+
+  const drawer = (
+    <div>
+      <div className={classes.toolbar} />
+      <Typography variant="h6">THE NEW DEVELOPERS</Typography>
+      <Divider />
+      <List>
+        {/* 
+          Eventually the below logic should be replaced by a GraphQL query that pulls information
+          for all pages and then programmatically populates them as list items with links.
+         */}
+        {["Join our mailing list", "Events", "Mentorship", "Opportunities"].map(
+          (text, index) => (
+            <ListItem button key={text}>
+              <ListItemText primary={text} />
+            </ListItem>
+          )
+        )}
+      </List>
     </div>
-  </header>
-)
+  )
 
-Header.propTypes = {
-  siteTitle: PropTypes.string,
-}
-
-Header.defaultProps = {
-  siteTitle: ``,
+  return (
+    <div>
+      <Hidden smUp implementation="css">
+        <AppBar position="fixed" className={classes.appBar}>
+          <Toolbar>
+            <IconButton
+              color="inherit"
+              aria-label="open drawer"
+              edge="start"
+              onClick={handleDrawerToggle}
+              className={classes.menuButton}
+            >
+              <Menu />
+            </IconButton>
+            <Typography variant="h6" noWrap>
+              THE NEW DEVELOPERS
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </Hidden>
+      <nav className={classes.drawer} aria-label="mailbox folders">
+        <Hidden smUp implementation="css">
+          <Drawer
+            variant="temporary"
+            anchor="left"
+            open={mobileOpen}
+            onClose={handleDrawerToggle}
+            classes={{
+              paper: classes.paper,
+            }}
+            ModalProps={{
+              keepMounted: true, // Better open performance on mobile.
+            }}
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+        <Hidden xsDown implementation="css">
+          <Drawer
+            classes={{
+              paper: classes.drawerPaper,
+            }}
+            variant="permanent"
+            open
+          >
+            {drawer}
+          </Drawer>
+        </Hidden>
+      </nav>
+    </div>
+  )
 }
 
 export default Header
--- website/src/components/layout.js
+++ website/src/components/layout.js
@@ -7,53 +7,17 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import {
-  AppBar,
-  Divider,
-  Drawer,
-  Hidden,
-  IconButton,
-  List,
-  ListItem,
-  ListItemText,
-  Toolbar,
-  Typography,
-} from "@material-ui/core"
-import MenuIcon from "@material-ui/icons/Menu"
-import { makeStyles, useTheme } from "@material-ui/core/styles"
-import { useStaticQuery, graphql } from "gatsby"
+import { Container } from "@material-ui/core"
+import { createMuiTheme, makeStyles } from "@material-ui/core/styles"
+import { ThemeProvider } from "@material-ui/styles"
+import Header from "./header"
 
 import "./layout.css"
-import { inherits } from "util"
-
-const drawerWidth = 240
 
 const useStyles = makeStyles(theme => ({
   root: {
     display: "flex",
     backgroundColor: "#0c0a05",
-  },
-  drawer: {
-    [theme.breakpoints.up("sm")]: {
-      width: drawerWidth,
-      flexShrink: 0,
-    },
-  },
-  appBar: {
-    marginLeft: drawerWidth,
-    [theme.breakpoints.up("sm")]: {
-      width: `calc(100% - ${drawerWidth}px)`,
-    },
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-    [theme.breakpoints.up("sm")]: {
-      display: "none",
-    },
-  },
-  toolbar: theme.mixins.toolbar,
-  drawerPaper: {
-    width: drawerWidth,
   },
   content: {
     flexGrow: 1,
@@ -61,104 +25,70 @@ const useStyles = makeStyles(theme => ({
   },
 }))
 
+const theme = createMuiTheme({
+  overrides: {
+    MuiPaper: {
+      root: {
+        "background-color": "#0c0a05",
+      },
+    },
+    MuiTypography: {
+      h1: {
+        "font-family": "Roboto Slab",
+      },
+      h2: {
+        "font-family": "Roboto Slab",
+      },
+      h3: {
+        "font-family": "Roboto Slab",
+      },
+      h4: {
+        "font-family": "Roboto Slab",
+      },
+      h5: {
+        "font-family": "Roboto Slab",
+      },
+      h6: {
+        "font-family": "Roboto Slab",
+      },
+    },
+  },
+  palette: {
+    type: "dark",
+    primary: {
+      light: "#FAFFF7", // white touched by green
+      main: "#0c0a05", // black touched by brown
+    },
+    secondary: {
+      main: "#3E5622", // green
+    },
+  },
+})
+
 /**
  * This is the Layout component, which will be called in all child components to provide
  * a unified style to the website.
- *
- * Much of this component has been drawn from the Material UI documentation for a responsive drawer,
- * which has a permanent drawer for tablet- and desktop-sized pages, and then a temporary drawer
- * for mobile pages.
- *
- * Reference: https://material-ui.com/components/drawers/#responsive-drawer
  */
 
 const Layout = ({ children }) => {
   const classes = useStyles()
-  const [mobileOpen, setMobileOpen] = React.useState(false)
-
-  function handleDrawerToggle() {
-    setMobileOpen(!mobileOpen)
-  }
-
-  const drawer = (
-    <div>
-      <div className={classes.toolbar} />
-      <Typography variant="h6">THE NEW DEVELOPERS</Typography>
-      <Divider />
-      <List>
-        {/* 
-          Eventually the below logic should be replaced by a GraphQL query that pulls information
-          for all pages and then programmatically populates them as list items with links.
-         */}
-        {["Join our mailing list", "Events", "Mentorship", "Opportunities"].map(
-          (text, index) => (
-            <ListItem button key={text}>
-              <ListItemText primary={text} />
-            </ListItem>
-          )
-        )}
-      </List>
-    </div>
-  )
 
   return (
-    <div className={classes.root}>
-      <Hidden smUp implementation="css">
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              THE NEW DEVELOPERS
-            </Typography>
-          </Toolbar>
-        </AppBar>
-      </Hidden>
-      <nav className={classes.drawer} aria-label="mailbox folders">
-        <Hidden smUp implementation="css">
-          <Drawer
-            variant="temporary"
-            anchor="left"
-            open={mobileOpen}
-            onClose={handleDrawerToggle}
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            ModalProps={{
-              keepMounted: true, // Better open performance on mobile.
-            }}
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-        <Hidden xsDown implementation="css">
-          <Drawer
-            classes={{
-              paper: classes.drawerPaper,
-            }}
-            variant="permanent"
-            open
-          >
-            {drawer}
-          </Drawer>
-        </Hidden>
-      </nav>
-      <div>
-        <main className={classes.content}>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
-        </footer>
+    <ThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <Header />
+        <div>
+          <Container>
+            <main className={classes.content}>{children}</main>
+          </Container>
+          <footer>
+            © {new Date().getFullYear()}, Built with
+            {` `}
+            <a href="https://www.gatsbyjs.org">Gatsby</a>
+          </footer>
+        </div>
       </div>
-    </div>
+    </ThemeProvider>
   )
 }
 

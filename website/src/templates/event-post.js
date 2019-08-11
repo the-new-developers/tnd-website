a33b9c47f1b8fb5b801c290ed45c6ec6e405b8c4
Rodney Barnes <rdnybrns@gmail.com>
2019-08-11 15:14:05 -0400

    Configured the event-post template.

--- website/src/templates/event-post.js
+++ website/src/templates/event-post.js
@@ -2,14 +2,22 @@ import React from "react"
 import { graphql, Link } from "gatsby"
 import Layout from "../components/layout"
 import { makeStyles } from "@material-ui/core/styles"
-import { Typography } from "@material-ui/core"
+import { Button, Typography } from "@material-ui/core"
 
 const useStyles = makeStyles(theme => ({
   body: {
     marginTop: theme.spacing(7),
   },
+  button: {
+    marginTop: theme.spacing(5),
+  },
   meta: {
     marginTop: theme.spacing(5),
+  },
+  eventInfo: {
+    [theme.breakpoints.down("xs")]: {
+      fontSize: "1em",
+    },
   },
   title: {
     [theme.breakpoints.down("xs")]: {
@@ -24,17 +32,41 @@ export default function EventPost({
   const { markdownRemark } = data // data.markdownRemark holds our post data
   const { frontmatter, html } = markdownRemark
   const classes = useStyles()
+  let path = frontmatter.link
+
+  // Gatsby seems to hijack all <a> tags and, if they do not begin with http://
+  // or https://, appends the path to the domain.
+  if (path && !path.includes("http://")) path = "http://" + path
+
   return (
     <Layout>
+      <Typography variant="overline">
+        {frontmatter.type === "TND" ? "TND Workshop" : "Community Event"}
+      </Typography>
       <Typography variant="h2" className={classes.title}>
         <Link to={markdownRemark.fields.slug}>{frontmatter.title}</Link>
       </Typography>
       <div className={classes.meta}>
-        <Typography variant="overline">DATE</Typography>
+        <Typography variant="h6" className={classes.eventInfo}>
+          When: {frontmatter.date}
+        </Typography>
+        <Typography variant="h6" className={classes.eventInfo}>
+          Where: {frontmatter.where}
+        </Typography>
       </div>
       <Typography variant="body1" className={classes.body}>
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </Typography>
+      {frontmatter.link ? (
+        <Button
+          variant="contained"
+          href={path}
+          color="secondary"
+          className={classes.button}
+        >
+          Register
+        </Button>
+      ) : null}
     </Layout>
   )
 }
@@ -49,6 +81,10 @@ export const pageQuery = graphql`
       }
       frontmatter {
         title
+        date(formatString: "h:mma dddd, MMMM Do YYYY")
+        where
+        link
+        type
       }
     }
   }

e13af7b64ea181ccc8f71ccec7fa1d1df395e22f
mitulvaghamshi <mitulvaghmashi@gmail.com>
2022-05-02 01:53:13 -0400

    #39 - Add pagination for [BlogList] and [EventList]
    Pagination created for [BlogList] and [EventList] pages...
    For [EventList] pagination is only applied to PastEvents as it will going to be a bigger list compared to UpcomingEvents.
    
    The [BlogList] and [EventList] class components were converted to functional components for easy pagination implementation.

---
+++ website/src/common/pagination.js
@@ -0,0 +1,50 @@
+import { TablePagination } from "@material-ui/core";
+import React from "react";
+
+/**
+ * This pagination component is implemented using
+ * [TablePagination] of material-ui library.
+ * A minimal pagination allowing previous(<), next(>),
+ * and number of items per page to display.
+ *
+ * Note: current implementation only works with functional components.
+ *
+ * Example:
+ * ```JavaScript
+ * // Using hooks, define two state variables
+ * // for [current-page] and [items-per-page] as:
+ * const [currentPage, setCurrentPage] = useState(0);
+ * const [rowsPerPage, setRowsPerPage] = useState(3);
+ *
+ * // Use [Pagination] component anywhare in component tree as:
+ * <Pagination count={posts.length}
+ *    currentPage={currentPage}
+ *    rowsPerPage={rowsPerPage}
+ *    setCurrentPage={setCurrentPage}
+ *    setRowsPerPage={setRowsPerPage} />
+ * ```
+ *
+ * // These params must be provided...
+ * @param {currentPage} currentPage State variable for current visible page.
+ * @param {rowsPerPage} rowsPerPage State variable for items per page.
+ * @param {setCurrentPage} setCurrentPage A function to update currentPage.
+ * @param {setRowsPerPage} setRowsPerPage A function to update rowsPerPage.
+ * @returns [TablePagination] component with given configurations.
+ */
+const Pagination = (props) => <TablePagination
+  page={props.currentPage}
+  component="div"
+  count={props.count}
+  rowsPerPage={props.rowsPerPage}
+  // on previous or next button clicked
+  onPageChange={(_, page) => props.setCurrentPage(page)}
+  // a dropdown with 3(default), 5, 8, and All items to display per page
+  rowsPerPageOptions={[3, 5, 8, {
+    value: props.count, label: 'All'
+  }]}
+  onRowsPerPageChange={e => {
+    props.setCurrentPage(0);
+    props.setRowsPerPage(e.target.value);
+  }} />;
+
+export default Pagination;
--- website/src/components/events/event-list.js
+++ website/src/components/events/event-list.js
@@ -1,21 +1,24 @@
-import React from "react"
+import { Divider, Typography } from "@material-ui/core"
 import { graphql, StaticQuery } from "gatsby"
 import PropTypes from "prop-types"
+import React, { useState } from "react"
 import EventListItem from "./event-list-item"
-import { Typography, Divider } from "@material-ui/core"
+import Pagination from "../../common/pagination";
 
-class EventList extends React.Component {
+const EventList = (props) => {
+  const [currentPage, setCurrentPage] = useState(0);
+  const [rowsPerPage, setRowsPerPage] = useState(3);
 
   /**
    * Partitions the given events into two arrays: upcoming events and past events.
    * @param {Array} events The events to partition.
    * @returns {Array} The partitioned events.
    */
-  partitionEvents(events){
+  const partitionEvents = (events) => {
     let upcomingEvents = [], pastEvents = []
     const dateToday = new Date()
 
-    for (let i = 0; i < events.length; i++){
+    for (let i = 0; i < events.length; i++) {
       if (new Date(events[i].node.frontmatter.date) >= dateToday)
         upcomingEvents.push(events[i])
       else
@@ -25,86 +28,75 @@ class EventList extends React.Component {
     return [upcomingEvents, pastEvents]
   }
 
-  render() {
-    const { data } = this.props
+  const { data } = props
 
-    const [upcomingEvents, pastEvents] = this.partitionEvents(data.allMarkdownRemark.edges)
+  const [upcomingEvents, pastEvents] =
+    partitionEvents(data.allMarkdownRemark.edges)
 
-    // We use Array.prototype.find() here because
-    // we are only expecting one featured post.
-    const featuredPost =
-      upcomingEvents &&
-      upcomingEvents.find(({ node }) => {
-        return node.frontmatter.featured
-      })
+  // We use Array.prototype.find() here because
+  // we are only expecting one featured post.
+  const featuredPost = upcomingEvents && upcomingEvents
+    .find(({ node }) => node.frontmatter.featured)
 
-    return (
-      <div>
-        <Typography
-          variant="h4"
-          component="h2"
-          style={{ fontWeight: "300", color: "#FAFFF7" }}
-        >
-          Events
-        </Typography>
-        {featuredPost ? (
-          <div>
-            <EventListItem
-              path={featuredPost.node.fields.slug}
-              info={featuredPost.node}
-            />{" "}
-            { upcomingEvents.length > 1 
-              ? 
-              <div>
-                <Divider style={{ marginTop: "50px", color: "#709255"}} /> 
-                <Typography
-                  variant="h5"
-                  component="h3"
-                  style={{ fontWeight: "300", color: "#FAFFF7" }}
-                >
-                  Upcoming Events
-                </Typography>
-              </div>
-              : null 
-            }
+  return (
+    <div>
+      <Typography
+        variant="h4"
+        component="h2"
+        style={{ fontWeight: "300", color: "#FAFFF7" }}
+      >Events</Typography>
+
+      {featuredPost ? (
+        <div>
+          <EventListItem
+            path={featuredPost.node.fields.slug}
+            info={featuredPost.node}
+          />{" "}
+          {upcomingEvents.length > 1 ? <div>
+            <Divider style={{ marginTop: "50px", color: "#709255" }} />
+            <Typography
+              variant="h5"
+              component="h3"
+              style={{ fontWeight: "300", color: "#FAFFF7" }}
+            >Upcoming Events</Typography>
+          </div> : null}
+        </div>) : null}
+
+      <Pagination count={pastEvents.length}
+        currentPage={currentPage}
+        rowsPerPage={rowsPerPage}
+        setCurrentPage={setCurrentPage}
+        setRowsPerPage={setRowsPerPage} />
+
+      {upcomingEvents && upcomingEvents.map(({ node }) => {
+        return (
+          <div key={node.id}>
+            {node.frontmatter.featured ? null : (
+              <EventListItem path={node.fields.slug} info={node} />
+            )}
           </div>
-        ) : null}
-        
-        {upcomingEvents &&
-          upcomingEvents.map(({ node }) => {
-            return (
-              <div key={node.id}>
-                {node.frontmatter.featured ? null : (
-                  <EventListItem path={node.fields.slug} info={node} />
-                )}
-              </div>
-            )
+        )
+      })}
+
+      {upcomingEvents.length === 0
+        ? <Typography variant="body1" style={{ fontStyle: "italic", marginTop: "50px" }}>There are no upcoming events currently scheduled.</Typography> : null}
+
+      <Divider style={{ marginTop: "50px", color: "#709255" }} />
+
+      {pastEvents ? <Typography
+        variant="h5"
+        component="h3"
+        style={{ fontWeight: "300", color: "#FAFFF7" }}
+      >Past Events</Typography> : null}
+
+      {pastEvents && pastEvents.slice(currentPage * rowsPerPage, currentPage * rowsPerPage + rowsPerPage)
+        .map(({ node }) => {
+          return (<div key={node.id}>
+            <EventListItem path={node.fields.slug} info={node} />
+          </div>)
         })}
-        { upcomingEvents.length === 0 
-          ? <Typography variant="body1" style={{ fontStyle: "italic", marginTop: "50px" }}>There are no upcoming events currently scheduled.</Typography> 
-          : null}
-        <Divider style={{ marginTop: "50px", color: "#709255"}} />
-        {pastEvents ? 
-          <Typography
-            variant="h5"
-            component="h3"
-            style={{ fontWeight: "300", color: "#FAFFF7" }}
-          >
-            Past Events
-          </Typography> 
-          : null
-        }
-          {pastEvents &&
-            pastEvents.map(({ node }) => {
-              return (
-                <div key={node.id}>
-                  <EventListItem path={node.fields.slug} info={node} />
-                </div>
-              )
-          })}
-      </div>
-    )
-  }
+    </div>
+  )
 }
 
 // propTypes allows us to define what type we expect the props to be.
@@ -120,7 +112,7 @@ EventList.propTypes = {
 
 // StaticQuery must be used in all components outside the pages directory.
 // These static GraphQL queries can be placed anywhere in the codebase.
-export default () => (
+const Query = () => (
   <StaticQuery
     query={graphql`
       query EventListQuery {
@@ -155,4 +147,6 @@ export default () => (
     `}
     render={data => <EventList data={data} />}
   />
-)
+);
+
+export default Query;
